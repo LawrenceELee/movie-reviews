691d5b738ec2cb1c2dbd8bf61241f2da51b1d4d2
Lawrence L <LawrenceELee@gmail.com>
2022-03-05 20:28:43 -0800

    Fix the associations problem. It was caused by a typo (adding an 's' to movie). It renders all reviews for a movie in the show.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,7 +3,8 @@ class MoviesController < ApplicationController
 
   # GET /movies or /movies.json
   def index
-    @movies = Movie.all
+    #@movies = Movie.all
+    @movies = Movie.all.order("created_at DESC")
   end
 
   # GET /movies/1 or /movies/1.json
